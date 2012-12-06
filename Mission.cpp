9bdec1ab96b5a0d22cf5a5bfaa92d40bbe8fde1e
Maverick Mosher <MMavipc@hostedyou.com>
2012-12-05 18:00:06 -0800

    Added real parsing, removed fake

--- Mission.cpp
+++ Mission.cpp
@@ -1,5 +1,6 @@
 #include "Mission.h"
 #include "StringFuncs.h"
+#include <iostream>
 
 Mission::Mission() : m_intel(NULL)
 {
@@ -7,8 +8,39 @@ Mission::Mission() : m_intel(NULL)
 
 void Mission::DeserializeSQM(std::istream &in)
 {
-	//testing only
-	SeekToPhrase("class Intel", in);
-	Intel intel;
-	intel.DeserializeSQM(in);
+	SeekToPhrase("class Mission", in);
+	SeekToPhrase("{", in);
+	while(!in.eof())
+	{
+		std::string strLine;
+		std::getline(in, strLine);
+		strLine = RemoveWhitespace(strLine);
+		//prepare for the madness
+		if(strLine == "addOns[]=")
+		{
+			while(RemoveWhitespace(strLine) != "};") //skip it for now
+			{
+				std::getline(in, strLine);
+			}
+		}
+		else if(strLine == "addOnsAuto[]=")
+		{
+			while(RemoveWhitespace(strLine) != "};") //same thing
+			{
+				std::getline(in, strLine);
+			}
+		}
+		else if(strLine == "class Intel")
+		{
+			if(m_intel != NULL)
+			{
+				std::cout << "WARNING: DUPLICATE INTEL";
+			}
+			else
+			{
+				m_intel = new Intel;
+			}
+			m_intel->DeserializeSQM(in);
+		}
+	}
 }
