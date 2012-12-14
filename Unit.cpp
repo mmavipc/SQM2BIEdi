061e21f14d197a2b0444c9c84b8d37ab6c32af35
Maverick Mosher <MMavipc@hostedyou.com>
2012-12-14 14:33:55 -0800

    Added position parsing to units

--- Unit.cpp
+++ Unit.cpp
@@ -1,4 +1,7 @@
 #include "Unit.h"
+#include <string>
+
+#include "StringFuncs.h"
 
 Unit::Unit(Group *grp)
 {
@@ -6,4 +9,33 @@ Unit::Unit(Group *grp)
 
 void Unit::DeserializeSQM(std::istream &in)
 {
+	std::string strLine;
+	std::getline(in, strLine);
+	unsigned short scope = 1;
+
+	while(scope != 0)
+	{
+		std::getline(in, strLine);
+
+		scope += CharCount(strLine, '{');
+		scope -= CharCount(strLine, '}');
+
+		size_t eqPos = strLine.find('=');
+		std::string strCmd = RemoveWhitespace(strLine.substr(0, eqPos));
+		std::string strArg = strLine.substr(eqPos+1, strLine.size()-1);
+		if(strCmd == "position[]")
+		{
+			strArg = StringReplace(strArg, "{", "");
+			size_t commaPos = strArg.find(',');
+			m_dX = atof(strArg.substr(0, commaPos).c_str());
+
+			strArg = strArg.substr(commaPos+1, strArg.length());
+			commaPos = strArg.find(',');
+			m_dZ = atof(strArg.substr(0, commaPos).c_str());
+
+			strArg = strArg.substr(commaPos+1, strArg.length());
+			commaPos = strArg.find("};");
+			m_dY = atof(strArg.substr(0, commaPos).c_str());
+		}
+	}
 }
--- Unit.h
+++ Unit.h
@@ -10,4 +10,8 @@ class Unit
 		Unit(Group *grp);
 		void DeserializeSQM(std::istream &in);
 		void SerializeBiEdi(std::ostream &out);
+	private:
+		double m_dX;
+		double m_dY;
+		double m_dZ;
 };
