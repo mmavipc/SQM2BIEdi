16841d339271bbaafcff5f1e37ec56cffff73f3d
Maverick Mosher <MMavipc@hostedyou.com>
2012-12-14 23:06:12 -0800

    Now writes to std::cout and a file at same time

--- SQM2BIEdi.cpp
+++ SQM2BIEdi.cpp
@@ -3,9 +3,43 @@
 
 #include <fstream>
 #include <iostream>
+#include <ostream>
 #include <string>
 
 #include "Mission.h"
+
+class MyBuf : public std::streambuf
+{
+	private:
+		std::fstream fout;
+	public:
+		MyBuf() : std::streambuf(), fout()
+		{
+			fout.open("mission.biedi", std::ios_base::out | std::ios_base::trunc);
+		}
+		~MyBuf()
+		{
+			std::cout.flush();
+			fout.close();
+		}
+	protected:
+		virtual traits_type::int_type overflow(traits_type::int_type c = traits_type::eof())
+		{
+			if(c != EOF)
+			{
+				std::cout << (char)c;
+				fout << (char)c;
+			}
+			return 0;
+		}
+
+		virtual std::streamsize xsputn ( const char * s, std::streamsize n )
+		{
+			std::cout.write(s, n);
+			fout.write(s, n);
+			return n;
+		}
+};
 
 int main(int argc, char* argv[])
 {
@@ -15,6 +49,8 @@ int main(int argc, char* argv[])
 	inFile.open("mission.sqm", std::ios_base::in);
 
 	mis.DeserializeSQM(inFile);
-	mis.SerializeBiEdi(std::cout);//std::cout for testing purposes
+	std::ostream os(new MyBuf());
+	os << 't' << "test" << std::endl;
+	mis.SerializeBiEdi(os);//std::cout for testing purposes
 	return 0;
 }
