0fe419e2c22447d7ace04c50aca025c45521469d
Maverick Mosher <MMavipc@hostedyou.com>
2012-12-15 01:15:47 -0800

    Waypoint deserialization

--- Wapoint.h
+++ Wapoint.h
@@ -9,7 +9,7 @@ class Group;
 class Waypoint
 {
 	public:
-		static std::vector<Waypoint*> DeserializeWaypoints(std::istream &in);
+		static std::vector<Waypoint*> DeserializeWaypoints(std::istream &in, Group *group);
 		Waypoint(Group *grp, unsigned short ID);
 		void DeserializeSQM(std::istream &in);
 		void SerializeBiEdi(std::ostream &out);
@@ -17,4 +17,14 @@ class Waypoint
 		Group *m_grp;
 
 		unsigned short m_ID;
+
+		double m_dX;
+		double m_dY;
+		double m_dZ;
+
+		std::string m_strCombatMode;
+		std::string m_strFormation;
+		std::string m_strSpeed;
+		std::string m_strCombat;
+		std::string m_strShowWP;
 };
--- Waypoint.cpp
+++ Waypoint.cpp
@@ -3,7 +3,7 @@
 
 #include "StringFuncs.h"
 
-std::vector<Waypoint*> Waypoint::DeserializeWaypoints(std::istream &in)
+std::vector<Waypoint*> Waypoint::DeserializeWaypoints(std::istream &in, Group *group)
 {
 	std::vector<Waypoint*> ret;
 	std::string strLine;
@@ -15,13 +15,87 @@ std::vector<Waypoint*> Waypoint::DeserializeWaypoints(std::istream &in)
 	strLine = StringReplace(strLine, ";", "");
 	ret.reserve(atoi(strLine.c_str()));
 
+	unsigned int scope = 1;
+	while(scope != 0)
+	{
+		std::getline(in, strLine);
+		scope += CharCount(strLine, '{');
+		scope -= CharCount(strLine, '}');
+		strLine = StringReplace(strLine, "\x09", "");
+		if(strLine.find("class Item") != std::string::npos)
+		{
+			ret.resize(ret.size()+1);
+			static unsigned short id = 0;
+			Waypoint *wp = new Waypoint(group, id);
+			id++;
+			wp->DeserializeSQM(in);
+			ret[ret.size()-1] = wp;
+		}
+	}
+
 	return ret;
 }
 
-Waypoint::Waypoint(Group *grp, unsigned short ID): m_grp(grp), m_ID(ID)
+Waypoint::Waypoint(Group *grp, unsigned short ID): m_grp(grp), m_ID(ID), m_dX(0), m_dY(0), m_dZ(0), m_strCombatMode(),
+	m_strFormation(), m_strSpeed(), m_strCombat(), m_strShowWP()
 {
 }
 
 void Waypoint::DeserializeSQM(std::istream &in)
 {
+	std::string strLine;
+	std::getline(in, strLine);
+	unsigned short scope = 1;
+
+	while(scope != 0)
+	{
+		std::getline(in, strLine);
+
+		scope += CharCount(strLine, '{');
+		scope -= CharCount(strLine, '}');
+
+		size_t eqPos = strLine.find('=');
+		std::string strCmd = RemoveWhitespace(strLine.substr(0, eqPos));
+		std::string strArg = strLine.substr(eqPos+1, strLine.size()-1);
+		if(strCmd == "position[]")
+		{
+			strArg = StringReplace(strArg, "{", "");
+			size_t commaPos = strArg.find(',');
+			m_dX = atof(strArg.substr(0, commaPos).c_str());
+
+			strArg = strArg.substr(commaPos+1, strArg.length());
+			commaPos = strArg.find(',');
+			m_dZ = atof(strArg.substr(0, commaPos).c_str());
+
+			strArg = strArg.substr(commaPos+1, strArg.length());
+			commaPos = strArg.find("};");
+			m_dY = atof(strArg.substr(0, commaPos).c_str());
+		}
+		else if(strCmd == "combatMode")
+		{
+			m_strCombatMode = strArg.substr(1, strArg.length()-3);
+		}
+		else if(strCmd == "formation")
+		{
+			m_strFormation = strArg.substr(1, strArg.length()-3);;
+		}
+		else if(strCmd == "speed")
+		{
+			m_strSpeed = strArg.substr(1, strArg.length()-3);;
+		}
+		else if(strCmd == "combat")
+		{
+			m_strCombat = strArg.substr(1, strArg.length()-3);;
+		}
+		else if(strCmd == "showWP")
+		{
+			m_strShowWP = strArg.substr(1, strArg.length()-3);;
+		}
+		else if(strCmd == "classEffects")
+		{
+			SeekToPhrase("{", in);// skip for now
+			SeekToPhrase("};", in);
+		}
+		//TODO: Mess around with all options and fill in the rest
+	}
 }
